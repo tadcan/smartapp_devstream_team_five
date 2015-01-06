fb3b84d2e7da0a2f484f161bae73438ada505ab7
John Prendergast <jprend@gmail.com>
2015-01-06 15:51:28 +0000

    commit 2nd go

---
+++ smartapp_team_five/src/io/devstream/smartapp_team_five/Appointment.java
@@ -0,0 +1,18 @@
+package io.devstream.smartapp_team_five;
+import java.util.Date;
+public class Appointment {
+    private int id;
+    private int service_provider_id;
+    private int service_user_id;
+    private int clinic_id;
+    private String 	priority;          
+    private String	visit_type;  
+    private Date	date_entered;
+    private Date	date;
+    
+	public Appointment() {
+		// TODO Auto-generated constructor stub	
+ 
+	}
+
+}
