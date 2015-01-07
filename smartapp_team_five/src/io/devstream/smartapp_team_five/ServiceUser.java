d5e2b16941cc1e30cc3be30e7da083a98f363c0d
Obaadewale <adewalejohnson.devstream@gmail.com>
2015-01-07 15:39:48 +0000

    gg

---
+++ smartapp_team_five/src/io/devstream/smartapp_team_five/ServiceUser.java
@@ -0,0 +1,17 @@
+package io.devstream.smartapp_team_five;
+//
+public class ServiceUser {
+	private String ClintName;
+	private String id;
+	private String address;
+	private String opening_time;
+	private String closing_time;
+	private String recurrence; 
+	private String type;
+	
+	public ServiceUser() {
+		// TODO Auto-generated constructor stub
+		
+	}
+ 
+}
