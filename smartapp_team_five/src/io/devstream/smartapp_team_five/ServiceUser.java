764710b86c16bc045e8be0aa8bb8ba6187c02b08
Obaadewale <adewalejohnson.devstream@gmail.com>
2015-01-08 16:06:16 +0000

    aa

--- smartapp_team_five/src/io/devstream/smartapp_team_five/ServiceUser.java
+++ smartapp_team_five/src/io/devstream/smartapp_team_five/ServiceUser.java
@@ -1,17 +1,51 @@
 package io.devstream.smartapp_team_five;
 //
 public class ServiceUser {
-	private String ClintName;
+	private String fname;
+	private String lname;
 	private String id;
 	private String address;
-	private String opening_time;
-	private String closing_time;
-	private String recurrence; 
-	private String type;
+ 	private int age;
+ 	private String pps;
+ 	private int contact;
 	
-	public ServiceUser() {
+	public ServiceUser(String theFname,String theLname, String theId, String theAddress, int theAge,String thePps, int theContact) {
 		// TODO Auto-generated constructor stub
-		
+		fname = theFname;
+    	lname = theLname;
+		pps = thePps;
+		age  = theAge;
+		address = theAddress;
+		contact = theContact;
+	}
+	public String getFname() {
+		return fname;
+	}
+	
+	public String getLname() {
+		return lname;
+	}
+	
+	public String getPps() {
+		return pps;
+	}
+	
+	public int getAge() {
+		return age;
+	}
+	
+	public String getAddress() {
+		return address;
+	}
+	public String getId() {
+		return id;
+	}
+	public int getContact() {
+		return contact;
+	}
+	
+	public String toString() {
+		return "\nName: "+ fname + " \nLast Name: " + lname + "\nThe address:" + address + "\nAge:" + age + "\nPPS:" + pps + "\nID:" + id + "\nContact:" + contact ;
 	}
  
-}
+ }
