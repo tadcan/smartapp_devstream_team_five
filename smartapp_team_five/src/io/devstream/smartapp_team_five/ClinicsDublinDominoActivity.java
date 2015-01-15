dd04cd74b1ca87e23ffc8bcedbebb03100b1b02e
tadcan <t4dc4n@gmail.com>
2015-01-15 15:28:17 +0000

    change MainActivity to LoginActivity

---
+++ smartapp_team_five/src/io/devstream/smartapp_team_five/ClinicsDublinDominoActivity.java
@@ -0,0 +1,15 @@
+package io.devstream.smartapp_team_five;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class ClinicsDublinDominoActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_clinics_dublin_domino);
+	}
+}
--- smartapp_team_five/src/io/devstream/smartapp_team_five/LoginActivity.java
+++ smartapp_team_five/src/io/devstream/smartapp_team_five/LoginActivity.java
@@ -3,12 +3,12 @@ package io.devstream.smartapp_team_five;
 import android.app.Activity;
 import android.os.Bundle;
 
-public class MainActivity extends Activity {
+public class LoginActivity extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+		setContentView(R.layout.activity_login);
 		
 		// im updated!!!!!!!!
 		
