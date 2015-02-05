c52ecfc7590579be391d498e09bf03cd26ecdde3
tadcan <t4dc4n@gmail.com>
2015-02-05 15:44:33 +0000

    eth clinic xml and java

---
+++ smartapp_team_five/src/io/devstream/smartapp_team_five/ClinicsETHWicklowActivity.java
@@ -0,0 +1,15 @@
+package io.devstream.smartapp_team_five;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class ClinicsETHWicklowActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_clinics_eth_wicklow);
+	}
+}
