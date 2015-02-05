00571f4de3a6cdc8c41215c56ad20925a9326449
tadcan <t4dc4n@gmail.com>
2015-02-05 18:14:23 +0000

    clinics ETH Dublin xml + java

--- smartapp_team_five/src/io/devstream/smartapp_team_five/ClinicsETHDublinActivity.java
+++ smartapp_team_five/src/io/devstream/smartapp_team_five/ClinicsETHDublinActivity.java
@@ -2,17 +2,14 @@ package io.devstream.smartapp_team_five;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
-public class LoginActivity extends Activity {
+public class ClinicsETHDublinActivity extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_login);
-		
+		setContentView(R.layout.activity_clinics_eth_dublin);
 	}
-	
-	// Todo add login in button
-	
-	// Check credentials against HS accounts
 }
