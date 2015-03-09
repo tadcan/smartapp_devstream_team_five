b8dbe0c9fe200a8d078412214f706ca6a8420556
Obaadewale <adewalejohnson.devstream@gmail.com>
2015-03-09 15:05:48 +0000

    ade

--- smartapp_team_five/src/io/devstream/smartapp_team_five/ServiceUserActivity.java
+++ smartapp_team_five/src/io/devstream/smartapp_team_five/ServiceUserActivity.java
@@ -1,10 +1,19 @@
 package io.devstream.smartapp_team_five;
 
+
+
 import android.app.Activity;
-import android.location.Address;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class ServiceUserActivity extends Activity {
+	   
+		private Button Edit;
+		private Button Ante_natal;
+		private Button Post_natal;
+		
 	
 
 	@Override
@@ -12,12 +21,52 @@ public class ServiceUserActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_service_user);
 		
+		  	  
+		    Edit = (Button)findViewById(R.id.label_2 );
+		    Edit.setOnClickListener(new ButtonClick());
+		    
+			Ante_natal = (Button)findViewById(R.id.label_3 );
+			Ante_natal.setOnClickListener(new ButtonClick());
+			
+			Post_natal = (Button)findViewById(R.id.label_4 );
+			Post_natal.setOnClickListener(new ButtonClick());
 		
-		
-		
-		
+			
+				
 	}
 	
+private class ButtonClick implements View.OnClickListener {
+		
+		public void onClick(View v) {
+		
+			switch( v.getId() ) {
+			
+			case R.id.label_2:
+				// launch intent 
+				
+			break;
+			
+			case R.id.label_3:
+				// launch intent 
+				
+			break;
+			
+			case R.id.label_4:
+				// launch intent 
+			
+			default:
+						
+							
+			}			
+			
+			
+		}
+			
+		
+	}
+}		
 	
-}
+
+	
+
 
